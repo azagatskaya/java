a6db6f8ea170d81edb6b4eb297002dfd30db598a
azagatskaya <61882557+azagatskaya@users.noreply.github.com>
2020-03-23 15:15:50 +0200

    Update TreeSet.java

--- TreeSet.java
+++ TreeSet.java
@@ -4,8 +4,6 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.function.Predicate;
 
-import org.omg.CORBA.Current;
-
 public class TreeSet<T> implements Set<T> {
 	private static class Node<T> {
 		T obj;
@@ -33,8 +31,8 @@ public class TreeSet<T> implements Set<T> {
 
 	@Override
 	public Iterator<T> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return new TreeIterator();
 	}
 
 	@Override
@@ -57,7 +55,7 @@ public class TreeSet<T> implements Set<T> {
 		}
 		size++;
 		newNode.parent = parent;
-		return false;
+		return true;
 	}
 
 	private void addRoot(T obj) {
@@ -67,21 +65,23 @@ public class TreeSet<T> implements Set<T> {
 	}
 
 	private Node<T> getParent(T obj) {
+
 		// beginning from the root we are going either to left or to right
 		// going to left -> current = current.left
 		// going to right -> current = current.right
 		// node from which you have come to null will be parent
 		Node<T> current = root;
-		Node<T> prevNode = current;
-		while(current != null) {
-			prevNode = current;
-			if (comparator.compare(obj, current.obj) < 0 && obj.equals(current.obj)) {
-				current = current.left;
-			} else {
-				current = current.right;
+		Node<T> parent = null;
+		while (current != null) {
+			parent = current;
+			int resComp = comparator.compare(obj, current.obj);
+			if (resComp == 0) {
+				parent = null;
+				break;
 			}
+			current = resComp < 0 ? current.left : current.right;
 		}
-		return prevNode;
+		return parent;
 	}
 
 	@Override
@@ -92,8 +92,28 @@ public class TreeSet<T> implements Set<T> {
 
 	@Override
 	public Object remove(Object pattern) {
-		// TODO Auto-generated method stub
-		return null;
+		Object res = null;
+		Node<T> node = findNode(pattern);
+		if (node != null) {
+			res = node.obj;
+			removeNode(node);
+		}
+		return res;
+	}
+
+	private Node<T> findNode(Object pattern) {
+		
+		Node<T> current = root;
+		Node<T> foundNode = null;
+		while (current != null) {
+			int resComp = comparator.compare((T) pattern, current.obj);
+			if (resComp == 0) {
+				foundNode = current;
+				break;
+			}
+			current = resComp < 0 ? current.left : current.right;
+		}
+		return foundNode;
 	}
 
 	@Override
@@ -104,14 +124,70 @@ public class TreeSet<T> implements Set<T> {
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return size;
 	}
 
 	@Override
 	public boolean contains(T pattern) {
-		// TODO Auto-generated method stub
-		return false;
+		return size > 0 && getParent(pattern) == null;
+	}
+
+	private Node<T> getLeastNode(Node<T> node) {
+		Node<T> current = node;
+		while (current.left != null) {
+			current = current.left;
+		}
+		return current;
+	}
+
+	private Node<T> getParentFromLeft(Node<T> node) {
+		while (node.parent != null && node.parent.right == node) {
+			node = node.parent;
+		}
+		return node.parent;
+	}
+	private void removeNode(Node<T> node) {
+		if (isJunction(node)) {
+			Node<T> substitute = getLeastNode(node.right);
+			node.obj = substitute.obj;
+			node = substitute;
+		}
+		removeNonJunctionNode(node);
+		size--;
+	}
+
+	private void removeNonJunctionNode(Node<T> node) {
+		if(node.parent.left == node) {
+			node.parent.left = node.right;
+		} else {
+			node.parent.right = node.right;
+		}
+	}
+
+	private boolean isJunction(Node<T> node) {
+		return node.left != null && node.right != null;
+	}
+
+	private class TreeIterator implements Iterator<T> {
+		Node<T> current = getLeastNode(root);
+
+		@Override
+		public boolean hasNext() {
+			return current != null;
+		}
+
+		@Override
+		public T next() {
+			T res = current.obj;
+			current = current.right != null ? getLeastNode(current.right) :
+				getParentFromLeft(current);
+			return res;
+		}
+
+		@Override
+		public void remove() {
+			// TODO
+		}
 	}
 
 }
