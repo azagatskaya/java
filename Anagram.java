c58ff8eef2aa4246c6742038f2af978f640b96d4
azagatskaya <61882557+azagatskaya@users.noreply.github.com>
2020-05-11 20:22:44 +0300

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -1,8 +1,6 @@
 package telran.text;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 
 public class Anagram {
 	public static boolean isAnagram(String word, String anagram) {
